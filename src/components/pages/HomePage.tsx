9adf828a341d305edc150fee296fb78d39df06b6
Wix Remote Machine <support@wix.com>
2026-03-04 07:14:45 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -419,7 +419,7 @@ export default function HomePage() {
                           </div>
                           <div className="relative h-64 md:h-auto overflow-hidden">
                             <Image
-                              src="https://static.wixstatic.com/media/db083c_90987eb1c29840fc8e0f483e37ad2307~mv2.png?originWidth=576&originHeight=576"
+                              src=""
                               alt={category.items[0].name}
                               className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
                               width={600}
